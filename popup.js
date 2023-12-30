707e285c637520b55e0d119a4ebb692686db31fb
Abdeali <abdealichharchhoda786@gmail.com>
2023-12-30 20:08:21 +0530

    clear all done

--- popup.js
+++ popup.js
@@ -2,17 +2,24 @@ const colorPickerBtn = document.querySelector("#color-picker");
 const colorList = document.querySelector(".all-colors");
 const clear_color = document.querySelector('.clear-all');
 const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
+const pickedColorElement = document.querySelector('.picked-colors');
 
 
 const copyColor = (element) => {
     navigator.clipboard.writeText(element.dataset.color);
-    
-    element.innerText="Copied";
 
-    setTimeout(()=> element.innerText = element.dataset.color,1000);
+    element.innerText = "Copied";
+
+    setTimeout(() => element.innerText = element.dataset.color, 1000);
 }
 
 const showColors = () => {
+
+    // if no colors show default bg
+
+    if (!pickedColors.length) {
+        return;
+    }
 
     // generating li for picked color and adding it to colorList
 
@@ -22,6 +29,12 @@ const showColors = () => {
             <span class="value" data-color="${color}">${color}</span>
          </li>`
     ).join("");
+    
+    // removing default image
+    document.querySelector('.default_bg_msg').style.display="none";
+
+    // showing element (Visible DOM)
+    pickedColorElement.classList.remove("hide");
 
     // add a click event listener to each color element to copy the color code
 
@@ -65,16 +78,21 @@ const activateEyeDropper = async () => {
 
 };
 
-const clearAllColors = () =>{
+const clearAllColors = () => {
 
     // removing from array
-    pickedColors.length=0;
+    pickedColors.length = 0;
 
     // removing from local storage
     localStorage.removeItem("picked-colors");
 
     // removing from dom
-    colorList.innerHTML="";
+    pickedColorElement.classList.add("hide");
+
+        
+    // removing default image
+    document.querySelector('.default_bg_msg').style.display="flex";
+
 }
 
 colorPickerBtn.addEventListener("click", activateEyeDropper);
